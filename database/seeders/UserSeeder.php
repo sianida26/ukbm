7f86c279ce7bf92905e47c65804f77af1bf29dee
sianida26 <chesakeselekkebo@gmail.com>
2021-12-17 03:55:42 +0700

    Buat 10 user (pak ridwan, mas deni, firda, siswa1-8)

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,14 +17,68 @@ class UserSeeder extends Seeder
     {
         $userNames = [
             [
-                'name' => 'guru',
-                'username' => 'guru',
-                'password' => 'StdPwdGuru2021',
+                'name' => 'Firda Amalia',
+                'username' => 'firda',
+                'password' => 'Oksigen8',
                 'roles' => ['guru'],
             ],
             [
-                'name' => 'siswa',
-                'username' => 'siswa',
+                'name' => 'Deni Ainur Rokhim, S.Pd.',
+                'username' => 'deni.ainur',
+                'password' => 'ukbm2021',
+                'roles' => ['guru'],
+            ],
+            [
+                'name' => 'Drs. Ridwan Joharmawan, M.Si.',
+                'username' => 'ridwan.joharmawan',
+                'password' => 'ukbm2021',
+                'roles' => ['guru'],
+            ],
+            [
+                'name' => 'Siswa 1',
+                'username' => 'siswa1',
+                'password' => 'StdPwdSiswa2021',
+                'roles' => ['siswa'],
+            ],
+            [
+                'name' => 'Siswa 2',
+                'username' => 'siswa2',
+                'password' => 'StdPwdSiswa2021',
+                'roles' => ['siswa'],
+            ],
+            [
+                'name' => 'Siswa 3',
+                'username' => 'siswa3',
+                'password' => 'StdPwdSiswa2021',
+                'roles' => ['siswa'],
+            ],
+            [
+                'name' => 'Siswa 4',
+                'username' => 'siswa4',
+                'password' => 'StdPwdSiswa2021',
+                'roles' => ['siswa'],
+            ],
+            [
+                'name' => 'Siswa 5',
+                'username' => 'siswa5',
+                'password' => 'StdPwdSiswa2021',
+                'roles' => ['siswa'],
+            ],
+            [
+                'name' => 'Siswa 6',
+                'username' => 'siswa6',
+                'password' => 'StdPwdSiswa2021',
+                'roles' => ['siswa'],
+            ],
+            [
+                'name' => 'Siswa 7',
+                'username' => 'siswa7',
+                'password' => 'StdPwdSiswa2021',
+                'roles' => ['siswa'],
+            ],
+            [
+                'name' => 'Siswa 8',
+                'username' => 'siswa8',
                 'password' => 'StdPwdSiswa2021',
                 'roles' => ['siswa'],
             ],
